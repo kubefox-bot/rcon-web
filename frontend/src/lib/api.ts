369d88c256cffc619bcf0765286dc1b880f94dce
xymox72 <skad72@live.ru>
2025-03-29 17:30:00 +0400

    feat: api

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,7 @@ import { config } from '@/config'
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: `${config.apiHost}:${config.apiPort}`,
   headers:{
     Authorization: `Bearer ${config.token}`
   }
