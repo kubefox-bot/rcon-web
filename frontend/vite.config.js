6fffd8772509358731052ec09bc7dda4023b2287
xymox72 <skad72@live.ru>
2025-04-06 16:16:51 +0400

    feat

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,24 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import path from 'path'
+import compression from 'vite-plugin-compression'
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [vue(), eslint()],
+    plugins: [vue(), eslint(),
+    compression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 1024,
+      deleteOriginFile: false,
+    })],
     preview: {
       host: true,
       allowedHosts: true
     },
-    
+
     server: {
       host: '0.0.0.0',
       port: Number(env.FRONT_PORT) || 5173,
@@ -40,5 +48,24 @@ export default defineConfig(({ mode }) => {
       FRONT_PORT: JSON.stringify(env.FRONT_PORT),
       FRONT_HOST: JSON.stringify(env.FRONT_HOST),
     },
+    build: {
+      manifest: true,
+      target: 'esnext',
+      outDir: 'dist',
+      assetsDir: 'assets',
+      cssCodeSplit: true,
+      sourcemap: false,
+      minify: 'esbuild',
+      reportCompressedSize: false,
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+          entryFileNames: 'assets/[name].[hash].js',
+          chunkFileNames: 'assets/[name].[hash].js',
+          assetFileNames: 'assets/[name].[hash][extname]',
+        },
+      },
+    },
+
   }
 })
